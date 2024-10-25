9435fd5a6e56000729259669709928375272d810
Jyf0214 <jyf20100214@outlook.com>
2024-10-26 06:55:37 +0800

    更新 config.ts

--- src/config.ts
+++ src/config.ts
@@ -71,7 +71,7 @@ const GLOBAL_CONFIG = {
   /**
    * 背景图片地址
    */
-  BACKGROUND_IMG_URL: "https://api.dujin.org/bing/1920.php",
+  BACKGROUND_IMG_URL: "https://bing.img.run/1920x1080.php",
   /**
    * ICP 备案号，留空不显示
    */
