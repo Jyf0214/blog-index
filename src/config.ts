a7be12833baac696455eb10dbc22307f44bea1a4
Jyf0214 <169313142+Jyf0214@users.noreply.github.com>
2024-10-26 10:59:12 +0800

    更新 config.ts

--- src/config.ts
+++ src/config.ts
@@ -16,9 +16,9 @@ const WEBSITE_SORTS: WebsiteSort[] = [
         icon: "",
       },
       {
-        title: "主标题",
-        description: "网站描述",
-        url: "http://www.your-web-page.com/",
+        title: "Lolicon APP",
+        description: "",
+        url: "https://jyf0214.giize.com/",
         icon: "",
       },
     ],
