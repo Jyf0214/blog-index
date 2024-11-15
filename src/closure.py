394983689df3c903d77af3f924c455f458ea2312
Jyf0214 <jyf20100214@outlook.com>
2024-11-16 05:14:32 +0800

    更新 closure.py

--- src/closure.py
+++ src/closure.py
@@ -104,7 +104,7 @@ def get_filtered_game_logs(logs_data):
 
         if any(keyword in log_content for keyword in keywords):
             filtered_logs.append(
-                f"<br>时间: {log_time}<br>日志: {log_content}（{log_name}）"
+                f"<br>时间: {log_time}<br>日志: {log_content}<br>"
             )
 
     return "<br>".join(filtered_logs) if filtered_logs else "<br>没有符合条件的日志<br>"
