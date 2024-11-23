c20b1c5b3d0dc44d6a065fbbf5bc4fe4b24438f1
Jyf0214 <169313142+Jyf0214@users.noreply.github.com>
2024-11-23 11:08:38 +0800

    更新 vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import { defineConfig } from "vite";
 import viteCompression from "vite-plugin-compression";
 import eslintPlugin from "vite-plugin-eslint";
+import { promises as fs } from "fs";
 
 // https://vitejs.dev/config/
 export default () => {
@@ -15,5 +16,30 @@ export default () => {
       },
     },
     envDir: "./",
+    build: {
+      rollupOptions: {
+        plugins: [
+          {
+            name: "move-files", // 自定义 Rollup 插件
+            async generateBundle() {
+              const sourceDir = path.resolve(__dirname, "src/assets");
+              const destDir = path.resolve(__dirname, "dist/assets");
+              
+              // 确保目标目录存在
+              await fs.mkdir(destDir, { recursive: true });
+
+              // 读取源目录并复制文件
+              const files = await fs.readdir(sourceDir);
+              for (const file of files) {
+                const srcFile = path.resolve(sourceDir, file);
+                const destFile = path.resolve(destDir, file);
+                await fs.copyFile(srcFile, destFile);
+                console.log(`Moved: ${file} -> ${destDir}`);
+              }
+            },
+          },
+        ],
+      },
+    },
   });
 };
