61619450f62cd7cd1ac2f6e783299b75cf573cb0
Jyf0214 <169313142+Jyf0214@users.noreply.github.com>
2025-01-05 09:53:23 +0800

    更新 random_string.py

--- src/random_string.py
+++ src/random_string.py
@@ -2,7 +2,7 @@ import random
 import string
 
 # 生成 16 位随机字符串
-random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
+random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=32))
 
 # 保存为 TXT 文件
 with open('random_string.txt', 'w') as f:
