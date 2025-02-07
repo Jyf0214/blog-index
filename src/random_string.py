f4757806eac6821938877fbf4f163cd21e020c03
Jyf0214 <169313142+Jyf0214@users.noreply.github.com>
2025-02-07 18:07:29 +0800

    更新 random_string.py

--- src/random_string.py
+++ src/random_string.py
@@ -5,5 +5,5 @@ import string
 random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=32))
 
 # 保存为 TXT 文件
-with open('random_string.txt', 'w') as f:
+with open('data.txt', 'w') as f:
     f.write(random_string)
